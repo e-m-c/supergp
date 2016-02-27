fce2dfa31759a3e41b0b793d0a146972ff480399
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-02-27 17:24:03 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,22 +1,7 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos, @iicc1 & @serx666      --
---                                              --
---------------------------------------------------
-
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout http://github.com/Desarrollo-Bots-Telegram-ES/DBTeam-Bot
-  --GNU GPL v2 license.]]
+  return 'EMC now work in supergroups\n\nadmins\n\n@shahabambesik\n\n@mr_bangi' 
 end
 
 return {
